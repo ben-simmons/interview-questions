666714da26b13799ba5ec2a60754a87ae36b55f0
Ben Simmons <simmons.a.ben@gmail.com>
2021-01-03 21:52:39 -0800

    Parameterize the tests by function to make it easy to write the tests once and then inject multiple solutions

--- dynamic_programming/test/test_dp_01_knapsack_problem.py
+++ dynamic_programming/test/test_dp_01_knapsack_problem.py
@@ -5,40 +5,46 @@ from dynamic_programming import dp_01_knapsack_problem as dp
 
 def naive_recurse(c, vals, wts):
     return dp.Knapsack(c, vals, wts).naive_recurse()
-    # return dp.naive_recurse_closure(c, vals, wts)
 
-def test_naive_recurse_exact_fit():
-    vals = [5, 5, 5, 5]
-    wts = [5, 5, 5, 5]
-    assert naive_recurse(20, vals, wts) == 20
+def naive_recurse_closure(c, vals, wts):
+    return dp.naive_recurse_closure(c, vals, wts)
 
-    wts = [1, 2, 3, 4]
-    assert naive_recurse(10, vals, wts) == 20
 
-def test_naive_recurse_no_fit():
-    vals = [5, 5, 5]
-    wts = [5, 5, 5]
-    assert naive_recurse(3, vals, wts) == 0
+@pytest.mark.parametrize('func', [naive_recurse, naive_recurse_closure])
+class TestKnapsack:
 
-def test_naive_recurse_fits_one():
-    vals = [4, 4, 4]
-    wts = [5, 5, 5]
-    assert naive_recurse(5, vals, wts) == 4
+    def test_fits_all_exactly(self, func):
+        vals = [5, 5, 5, 5]
+        wts = [5, 5, 5, 5]
+        assert func(20, vals, wts) == 20
 
-def test_naive_recurse_fits_last_element_in_array():
-    vals = [4, 5, 6]
-    wts = [5, 5, 5]
-    assert naive_recurse(5, vals, wts) == 6
+        wts = [1, 2, 3, 4]
+        assert func(10, vals, wts) == 20
 
-def test_naive_recurse_extra_space():
-    vals = [5, 5, 5]
-    wts = [5, 5, 5]
-    assert naive_recurse(20, vals, wts) == 15
+    def test_fits_all_extra_space(self, func):
+        vals = [5, 5, 5]
+        wts = [5, 5, 5]
+        assert func(20, vals, wts) == 15
 
-def test_naive_recurse_cant_fill():
-    vals = [1, 2, 3, 4]
-    wts = [10, 5, 3, 2]
-    assert naive_recurse(14, vals, wts) == 9
+    def test_fits_one_exactly(self, func):
+        vals = [4, 4, 4]
+        wts = [5, 5, 5]
+        assert func(5, vals, wts) == 4
 
-    vals = [10, 3, 2, 1]
-    assert naive_recurse(14, vals, wts) == 12
+    def test_fits_none(self, func):
+        vals = [5, 5, 5]
+        wts = [5, 5, 5]
+        assert func(3, vals, wts) == 0
+
+    def test_starts_filling_from_end_of_array(self, func):
+        vals = [4, 5, 6]
+        wts = [5, 5, 5]
+        assert func(5, vals, wts) == 6
+
+    def test_fits_highest_value(self, func):
+        vals = [1, 2, 3, 4]
+        wts = [10, 5, 3, 2]
+        assert func(14, vals, wts) == 9
+
+        vals = [10, 3, 2, 1]
+        assert func(14, vals, wts) == 12
